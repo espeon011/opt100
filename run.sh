7690b98a09e880317e0fb01976b66af3aa3fb36c
psiana011 <psiana011.endo@gmail.com>
2025-04-23 17:28:13 +0900

    実行方式を変更

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env sh
 
-uv run marimo --development-mode edit --headless --host 0.0.0.0 --sandbox --no-token
+# uv run marimo --development-mode edit --headless --host 0.0.0.0 --sandbox --no-token
+uv run marimo edit --headless --host 0.0.0.0 --sandbox --no-token
